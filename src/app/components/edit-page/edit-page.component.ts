34b9f9a01cbf6f7dc1bafe50b6c0338a6677df55
pennywisexy <54883206+pennywisexy@users.noreply.github.com>
2021-01-18 20:35:33 +0300

    made a changing movies

--- src/app/components/edit-page/edit-page.component.ts
+++ src/app/components/edit-page/edit-page.component.ts
@@ -11,6 +11,12 @@ export class EditPageComponent implements OnInit {
 
   form: FormGroup;
 
+  editBtn = false;
+
+  formData;
+
+  itemKey;
+
   constructor(public data?: GetDataService) { }
 
   ngOnInit(): void {
@@ -29,8 +35,34 @@ export class EditPageComponent implements OnInit {
   }
 
   submit(): void {
-    const formData = {...this.form.value};
-    this.data.movies.push(formData);
+    this.formData = {...this.form.value};
+    this.data.movies.push(this.formData);
+    this.form.reset();
+  }
+
+  editMovie(item): void {
+    this.editBtn = !this.editBtn;
+    this.itemKey = item.key;
+    this.form = new FormGroup({
+      description: new FormControl(item.value.description, Validators.required),
+      sources: new FormControl(item.value.sources, Validators.required),
+      thumb: new FormControl('https://render.fineartamerica.com/images/rendered/default/greeting-card/images-medium-5/captain-america-shield-digital-painting-georgeta-blanaru.jpg?&targetx=0&targety=-100&imagewidth=700&imageheight=700&modelwidth=700&modelheight=500&backgroundcolor=161718&orientation=0'),
+      title: new FormControl(item.value.title, Validators.required),
+      subtitle: new FormControl(item.value.subtitle, Validators.required)
+    });
+  }
+
+  editChangedMovie(): void {
+    const editingMovie = this.data.movies[this.itemKey];
+
+    this.editBtn = !this.editBtn;
+
+    editingMovie.description = this.form.value.description;
+    editingMovie.sources = this.form.value.sources;
+    editingMovie.title = this.form.value.title;
+    editingMovie.subtitle = this.form.value.subtitle;
+
+    this.form.reset();
   }
 
 }
